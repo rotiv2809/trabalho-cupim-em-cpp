4cb8878c3490721a6beaf25f6a841a9e505cac12
rotiv2809 <vitorpzie@gmail.com>
2024-04-14 15:41:59 -0300

    sexo

--- teste.cpp
+++ teste.cpp
@@ -12,6 +12,7 @@ public:
     string nome_disciplina;
     string professor;
     string creditos;
+    vector<class aluno> alunos_na_disciplina;
 };
 
 class aluno {
@@ -33,11 +34,13 @@ void adicionarDisciplina(aluno &aluno, int index) {
     cout << "Digite a quantidade de creditos da disciplina " << index + 1 << ": ";
     cin >> nova_disciplina.creditos;
     aluno.disciplinas_cursadas.push_back(nova_disciplina);
+    nova_disciplina.alunos_na_disciplina.push_back(aluno);
 }
 
 
-void removerDisciplina(aluno& aluno, int index) {
+void removerDisciplina(aluno& aluno, int index_aluno, int index) {
     if (index >= 0 && index < aluno.disciplinas_cursadas.size()) {
+        aluno.disciplinas_cursadas[index].alunos_na_disciplina.erase(aluno.disciplinas_cursadas[index].alunos_na_disciplina.begin() + index_aluno);
         aluno.disciplinas_cursadas.erase(aluno.disciplinas_cursadas.begin() + index);
         cout << "Disciplina removida com sucesso!\n";
     } else {
@@ -51,7 +54,7 @@ int main() {
         int opcao;
         int ano, periodo, numero_de_alunos, numero_de_disciplinas;
 
-        string sano, speriodo, titulo_txt;
+        string nome_de_arquivo, titulo_txt;
 
         cout << "\n---------- Cadastro de aluno -----------\n\n\n";
         cout << "Digite o ano em que se encontra para cadastrar o aluno: ";
@@ -61,12 +64,7 @@ int main() {
         cout << "Digite quantos alunos voce deseja cadastrar: ";
         cin >> numero_de_alunos;
         vector<aluno> alunos(numero_de_alunos);
-
-        sano = to_string(ano);
-        speriodo = to_string(periodo);
-        titulo_txt = sano + "_" + speriodo + ".txt";
-
-        ofstream teste(titulo_txt);
+        nome_de_arquivo = ano + '-' + periodo;
 
         for (int i = 0; i < numero_de_alunos; i++) {
             cout << "Cadastro do aluno " << i + 1 << ":\nQuantas disciplinas deseja cadastrar? ";
@@ -84,21 +82,14 @@ int main() {
             for (int j = 0; j < numero_de_disciplinas; j++) {
                 adicionarDisciplina(alunos[i], j);
             }
-
-            teste << "Aluno: " << alunos[i].nome_aluno << "\n    Codigo: " << alunos[i].codigo_aluno << "\n    cpf: " << alunos[i].cpf << "\n    Materias cursadas: \n";
-            for (int j = 0; j < alunos[i].disciplinas_cursadas.size(); j++) {
-                teste << "         Codigo: " << alunos[i].disciplinas_cursadas[j].codigo_disciplina << "\n            Nome: " << alunos[i].disciplinas_cursadas[j].nome_disciplina << "\n            Professor: " << alunos[i].disciplinas_cursadas[j].professor << "\n            Creditos: " << alunos[i].disciplinas_cursadas[j].creditos << "\n";
-            }
-            teste << "\n";
         }
-
-        teste.close();
 
         cout << "Menu de opções:\n";
         cout << "1. Adicionar aluno\n";
         cout << "2. Remover aluno\n";
         cout << "3. Adicionar disciplina a aluno\n";
         cout << "4. Remover disciplina de aluno\n";
+        cout << "5. Ver os alunos de uma disciplina\n";
         cout << "Escolha a opção desejada: ";
         cin >> opcao;
 
@@ -114,19 +105,21 @@ int main() {
                 // (mantive a funcionalidade existente de adicionar disciplina)
                 break;
             case 4:
-                int aluno_index, disciplina_index;
+                int aluno_index, disciplina_index, aluno_na_disciplina_index;
                 cout << "Digite o indice do aluno que deseja remover disciplina: ";
                 cin >> aluno_index;
                 cout << "Digite o indice da disciplina que deseja remover: ";
                 cin >> disciplina_index;
                 if (aluno_index >= 0 && aluno_index < alunos.size()) {
-                    removerDisciplina(alunos[aluno_index], disciplina_index);
+                    removerDisciplina(alunos[aluno_index], aluno_na_disciplina_index, disciplina_index);
                 } else {
                     cout << "Indice de aluno invalido.\n";
                 }
                 break;
             default:
                 cout << "Opcao invalida.\n";
+                break;
+            case 5:
                 break;
         }
 
